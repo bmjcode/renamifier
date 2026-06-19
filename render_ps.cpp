392bbf713078e88d1bac8e5f02ff299e931b0756
Benjamin Johnson <42893476+bmjcode@users.noreply.github.com>
2026-06-19 16:22:00 -0400

    Remove annoying trailing spaces

--- render_ps.cpp
+++ render_ps.cpp
@@ -1,17 +1,17 @@
 /*
  * Renderer for Postscript documents.
  * Copyright (c) 2021-2026 Benjamin Johnson
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
@@ -85,7 +85,7 @@ const QString findGhostscript()
             for (int j = 0; j < gsDirs.size(); ++j) {
                 // See what executables this installation provides
                 for (int k = 0; k < gsNames.size(); ++k) {
-                    QString gsExe = gsBaseDir.path() + "/" 
+                    QString gsExe = gsBaseDir.path() + "/"
                                     + gsDirs[j] + "/bin/" + gsNames[k];
                     if (QFileInfo(gsExe).isExecutable()) {
                         program = gsExe;
